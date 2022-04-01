02c3cc84cd37146532aa31ab55e5ee6a9aea8ddc
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-04-01 15:31:40 +0200

    Header template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     AuthenticationModule,
-    HttpClientModule
+    HttpClientModule,
+    SharedModule
   ],
   bootstrap: [AppComponent]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { InputComponent } from './input/input.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {InputComponent} from './input/input.component';
 import {ReactiveFormsModule} from "@angular/forms";
-
-
+import {HeaderComponent} from './header/header.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    InputComponent
+    InputComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   exports: [
-    InputComponent
+    InputComponent,
+    HeaderComponent
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
