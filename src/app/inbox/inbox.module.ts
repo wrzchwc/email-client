39696c47a30d486ce7a26f221ff738914b64c819
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-04-06 20:52:56 +0200

    Generated components and service

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
 import { InboxHomeComponent } from './inbox-home/inbox-home.component';
+import { EmailCreateComponent } from './email-create/email-create.component';
+import { EmailReplyComponent } from './email-reply/email-reply.component';
+import { EmailIndexComponent } from './email-index/email-index.component';
+import { EmailShowComponent } from './email-show/email-show.component';
 
 
 @NgModule({
   declarations: [
-    InboxHomeComponent
+    InboxHomeComponent,
+    EmailCreateComponent,
+    EmailReplyComponent,
+    EmailIndexComponent,
+    EmailShowComponent
   ],
   imports: [
     CommonModule,
