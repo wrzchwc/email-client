31e3d04ff05ca5b407278a1bcb3a7e13e5d96075
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-04-07 13:35:36 +0200

    Styling active email

--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {InboxHomeComponent} from "./inbox-home/inbox-home.component";
+import {PlaceholderComponent} from "./placeholder/placeholder.component";
+import {EmailShowComponent} from "./email-show/email-show.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: InboxHomeComponent
+    component: InboxHomeComponent,
+    children: [
+      {
+        path:':id',
+        component: EmailShowComponent
+      },
+      {
+        path: '',
+        component: PlaceholderComponent
+      }
+    ]
   }
 ];
 
