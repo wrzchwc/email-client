9529b48f6589936828ceae464ac79970b4396247
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-04-12 19:39:31 +0200

    Fixed layout

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges} from '@angular/core';
+import {Email} from "../interfaces/email";
+import {Input} from "@angular/core";
+import {EmailService} from "../services/email.service";
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
   styleUrls: ['./email-reply.component.css']
 })
-export class EmailReplyComponent implements OnInit {
+export class EmailReplyComponent implements OnChanges {
+  showModal: boolean;
+  @Input() email!: Email
 
-  constructor() { }
+  constructor(private emailService: EmailService) {
+    this.showModal = false;
+  }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    const text = this.email.text.replace(/\n/gi, '\n> ');
+
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `RE: ${this.email.subject}`,
+      text: `\n\n\n----${this.email.from} wrote:\n${text}`
+    }
+  }
+
+  onSubmit(email: Email) {
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false
+    })
   }
 
 }
